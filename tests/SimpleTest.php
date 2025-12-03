abcbf9ad543539b6067f75a637d07106f524780d
IneskSlg <i.kechaba@gmail.com>
2025-12-03 10:47:47 +0000

    test: ajout SimpleTest.php

---
+++ tests/SimpleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
