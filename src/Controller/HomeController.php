2a297dae79680f62276071bd31cd6b95b2c2f336
InesKslg <i.kechaba@gmail.com>
2026-02-17 12:02:49 +0100

    Fix docker-compose: Mercure config & PostgreSQL roles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,22 @@
 
 namespace App\Controller;
 
-use App\Repository\PhotosRepository; // <-- attention au nom exact
+use App\Repository\PhotosRepository; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(PhotosRepository $photosRepository): Response
     {
-        // Récupération des photos
+        // Récupère toutes les photos
         $photos = $photosRepository->findAll();
 
+        // Rendu du template index.html.twig
         return $this->render('home/index.html.twig', [
-            'photos' => $photos
+            'photos' => $photos,
         ]);
     }
 }
