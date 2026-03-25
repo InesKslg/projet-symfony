96a5a4ce01f499058a9e7c0b56105f121e177c27
InesKslg <i.kechaba@gmail.com>
2026-03-25 09:18:58 +0000

    rubrique album et affinites

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,44 +15,37 @@ class HomeController extends AbstractController
         ThemesRepository $themesRepository,
         PhotosRepository $photosRepository
     ): Response {
-        // Toutes les photos publiques
-        $allPhotos = $photosRepository->findBy(['public' => true], ['date_added' => 'DESC']);
 
         // Récupère tous les thèmes
         $allThemes = $themesRepository->findAll();
 
-        // Tri par date du dernier ajout d'une photo associée
+        // Tri par date du dernier ajout d'une photo associée (plus récent en premier)
         usort($allThemes, function($a, $b) {
             $lastPhotoA = $a->getPhotos()->last();
             $lastPhotoB = $b->getPhotos()->last();
-
             $dateA = $lastPhotoA ? $lastPhotoA->getDateAdded()->getTimestamp() : 0;
             $dateB = $lastPhotoB ? $lastPhotoB->getDateAdded()->getTimestamp() : 0;
-
-            return $dateB <=> $dateA; // plus récent en premier
+            return $dateB <=> $dateA;
         });
 
         // Prends les 3 thèmes les plus récents
         $recentThemes = array_slice($allThemes, 0, 3);
 
-        // Photos du premier thème (par défaut)
-        $defaultPhotos = [];
-        if (!empty($recentThemes)) {
-            $firstTheme = $recentThemes[0];
+        // Photos publiques à afficher
+        $allPublicPhotos = $photosRepository->findBy(['public' => true], ['date_added' => 'DESC']);
 
-            foreach ($firstTheme->getPhotos() as $photo) {
-                if ($photo->isPublic()) { // ⚡️ filtrer uniquement les publiques
-                    $defaultPhotos[] = [
-                        'photo' => $photo,
-                        'themeId' => $firstTheme->getId(),
-                    ];
-                }
-            }
+        // Tableau pour le Twig : photo + themeId (premier thème si existe)
+        $defaultPhotos = [];
+        foreach ($allPublicPhotos as $photo) {
+            $themeId = $photo->getThemes()->first() ? $photo->getThemes()->first()->getId() : null;
+            $defaultPhotos[] = [
+                'photo' => $photo,
+                'themeId' => $themeId,
+            ];
         }
 
         return $this->render('home/index.html.twig', [
             'recentThemes' => $recentThemes,
-            'allPhotos' => $allPhotos,
             'defaultPhotos' => $defaultPhotos,
             'firstTheme' => $recentThemes[0] ?? null,
         ]);
